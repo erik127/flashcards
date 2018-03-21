0c4f4974ff5b79371a2dcb743b1414873c49b196
Erik <erik127@hotmail.com>
2018-03-21 14:39:27 +0100

    logos added

--- src/components/stats.vue
+++ src/components/stats.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="stats">
-   <p>The start deck: <span class= 'float-right'> {{ deck0 }} </span></p>
-   <p>The 'Oops I got it wrong' deck: <span class= 'float-right'> {{ deck1 }} </span></p>
-   <p>The 'I think I know it' deck: <span class= 'float-right'> {{ deck2 }} </span></p>
-   <p>The 'I master this' deck: <span class= 'float-right'> {{ deck3 }} </span></p>
+    <div> 
+      <p>{{ deck0 }}</p>
+      <p>start</p>
+    </div>
+    <div> 
+      <p>{{ deck1 }}</p>
+      <p>wrong</p>
+    </div>
+    <div> 
+      <p>{{ deck2 }}</p>
+      <p>right</p>
+    </div>
+    <div> 
+      <p>{{ deck3 }}</p>
+      <p>done</p>
+    </div>
+   <!--  <p>The start deck: <span class= 'float-right'> {{ deck0 }} </span></p>
+    <p>The 'Oops I got it wrong' deck: <span class= 'float-right'> {{ deck1 }} </span></p>
+    <p>The 'I think I know it' deck: <span class= 'float-right'> {{ deck2 }} </span></p>
+    <p>The 'I master this' deck: <span class= 'float-right'> {{ deck3 }} </span></p> -->
   </div>
 </template>
 
@@ -37,6 +53,19 @@ export default {
 .stats {
   width: 15em;
   margin: 0 auto;
-  text-align: left;
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
+
+.stats div {
+  width: 20%;
+  height: 10%;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  text-align: center;
+
+}
+
 </style>
