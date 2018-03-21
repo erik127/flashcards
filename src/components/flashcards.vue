2df3e4c40f9dd545272e614a542129c48aaf0199
Erik <erik127@hotmail.com>
2018-03-21 10:49:48 +0100

    Cardflip implemented

--- src/components/flashcards.vue
+++ src/components/flashcards.vue
@@ -1,35 +1,39 @@
 <template>
-  <div class="flashcards">
-    <svg viewBox='0 0 20 20' class='back-button' v-if='status === "answer"' @click='flip'>
-        <title>back</title>
-        <circle cx='10' cy='10' r='9'/>
-        <path d="m8 9 -3 3 3 3"/>
-        <path d="m6 12c9-0 12-3 7-6"/>
-      </svg>
-    <p v-if='status === "question"' class='width-80 centered'>{{question}}</p>
-    <p v-if='status === "answer"' class='width-80 centered'>{{answer}}</p>
-    <div class='button-box width-80 centered'>
-<!--       <input type='button' value='flip' class='flip-button width-100' v-if='status === "question"' @click='flip'> --> 
-<!--       <div class='flip-button' v-if='status === "question"' @click='flip'>
-        <img src="/static/img/icons/arrow-right5.svg" class='width-100' alt="Flip">
-      </div> -->
-      <svg viewBox='0 0 20 20' class='flip' v-if='status === "question"' @click='flip'>
-        <title>arrow-right</title>
-        <circle cx='10' cy='10' r='9'/>
-        <path d='m3 10 h13'/>
-        <path d='m12 5 5 5 -5 5'/>
-      </svg>
-      <svg viewBox='0 0 20 20' class='wrong' v-if='status === "answer"' @click='wrong'>
-        <title>cross</title>
-        <circle cx='10' cy='10' r='9'/>
-        <path d='m5 5 10 10'/>
-        <path d='m5 15 10 -10'/>
-      </svg>
-      <svg viewBox='0 0 20 20' class='right' v-if='status === "answer"' @click='right'>
-        <title>checkmark</title>
-        <circle cx='10' cy='10' r='9'/>
-        <path d='M 4,11 9,16 15,5'/>
-      </svg>
+  <div class='container'>
+    <div class='flashcards' :class='{flipped: isAnswer}'>
+      <div class='front'>
+        <p class='question'>{{question}}</p>
+        <div class='button-box'>
+          <svg viewBox='0 0 20 20' class='flip' @click='flip'>
+            <title>arrow-right</title>
+            <circle cx='10' cy='10' r='9'/>
+            <path d='m3 10 h13'/>
+            <path d='m12 5 5 5 -5 5'/>
+          </svg>
+        </div>
+      </div>
+      <div class='back'>
+        <svg viewBox='0 0 20 20' class='back-button' @click='flip'>
+            <title>back</title>
+            <circle cx='10' cy='10' r='9'/>
+            <path d="m8 9 -3 3 3 3"/>
+            <path d="m6 12c9-0 12-3 7-6"/>
+          </svg>
+        <p class='answer'>{{answer}}</p>
+        <div class='button-box'>
+          <svg viewBox='0 0 20 20' class='wrong' @click='wrong'>
+            <title>cross</title>
+            <circle cx='10' cy='10' r='9'/>
+            <path d='m5 5 10 10'/>
+            <path d='m5 15 10 -10'/>
+          </svg>
+          <svg viewBox='0 0 20 20' class='right' @click='right'>
+            <title>checkmark</title>
+            <circle cx='10' cy='10' r='9'/>
+            <path d='M 4,11 9,16 15,5'/>
+          </svg>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,7 +44,8 @@ export default {
   // props: ['card'],
   data () {
     return {
-      status: 'question'
+      // status: 'question',
+      isAnswer: false
     }
   },
   computed: {
@@ -74,7 +79,14 @@ export default {
   },
   methods: {
     flip: function () {
-      this.status = this.status === 'question' ? 'answer' : 'question'
+      // this.status = this.status === 'question' ? 'answer' : 'question'
+      this.isAnswer = !this.isAnswer
+
+      // if (this.isAnswer === true) {
+      //   this.isAnswer = false
+      // } else {
+      //   this.isAnswer = true
+      // }
     },
     wrong: function () {
       this.$emit('answer', 'wrong')
@@ -90,38 +102,65 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.flashcards {
+.container {
+  perspective: 1000px;
+  margin: 3em auto;
   height: 20em;
   width: 15em;
-  margin: 3em auto;
-  padding: 5% 0;
-  border-radius: 10px;
-  text-align: left;
-  clear: both;
+}
+
+.flashcards,
+.front,
+.back {
+  height: 100%;
+  width: 100%;
+}
+
+.flashcards {
   position: relative;
+  transition: 0.6s;
+  transform-style: preserve-3d;
+  clear: both;
+}
+
+.flipped {
+  transform: rotateY(180deg);
+  /*box-shadow: -2px 2px 10px 0 rgba(0, 0, 0, 0.2);*/
+}
+
+.front,
+.back {
+  padding: 15%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  backface-visibility: hidden;
+  position: absolute;
+  box-sizing: border-box;
+  top: 0;
+  left: 0;
+  border-radius: 10px;
   box-shadow: 2px 2px 10px 0 rgba(0, 0, 0, 0.2);
+  background-color: #fff;
 }
+
+.back {
+  transform: rotateY(180deg);
+ }
+
+ .question,
+ .answer {
+  text-align: center;
+ }
 
 .button-box {
   align-self: flex-end;
   position: relative;
   display: flex;
-  align-items: center;
+/*  align-items: center;*/
+  width: 100%;
+  height: 30%;
 }
-
-/*.flip-button {
-  font-size: 2em;
-  width: 3em;
-  height: 3em;
-  margin: 0 auto;
-  border-radius: 50%;
-  background: -webkit-linear-gradient(#5ce, #5ce);
-  background: linear-gradient(#5ce,#5ce);
-  color: #fff;
-}*/
 
 .button-box svg {
   width: 6em;
@@ -147,10 +186,10 @@ export default {
   fill: #d41111;
 }
 
-.right {
+/*.right {
   position: absolute;
   right: 0;
-}
+}*/
 
 .right circle {
   fill: #11d42e;
@@ -171,33 +210,5 @@ export default {
 .back-button path {
   stroke: #bbb;
 }
-
-/*.answer-icon {
-  fill: none;
-  stroke: #fff;
-  stroke-linecap: round;
-  stroke-linejoin: round;
-  stroke-width: 2;
-}*/
-
-/*.answer-button {
-  font-size: 1.5em;
-  width: 4em;
-  height: 4em;
-  border-radius: 50%;
-  color: #fff;
-}*/
-
-
-/*
-.back-button {
-  margin: 0;
-  position: absolute;
-  right: 0.5em;
-  top: 0.5em;
-  color: #ccc;
-}*/
-
-
 
 </style>
