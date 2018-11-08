49e9a27fafa6bedd8d9ad8d15b5c27c59aa81a22
Erik <erik127@hotmail.com>
2018-11-08 22:49:37 +0100

    Updated help text

--- src/components/help.vue
+++ src/components/help.vue
@@ -1,8 +1,51 @@
 <template>
   <div>
-    <p>
+    <h3>
       How to use flashcards
+    </h3>
+    <p><strong>
+      Introduction
+    </strong></p>
+    <p>
+      Flashcards can be a simple and effective method to learn things. In the case of languages, each card will have a term on one side and it's translation on the other. With this app you don't need actual physical cards, I made them for you.
     </p>
+    <p><strong>
+      How to play
+    </strong></p>
+    <p>
+      Let's say you leave the default languages (from English to Spanish). You will be presented with your first card. Look at the term and think of the translation. If you're ready, click the blue button with arrow to see if you have the answer right. If you have it right, tap the green button with checkmark. If you have it wrong, you'll tap the red icon with a cross.
+    </p>
+    <p>
+      If it was right, the card will go to the third stack. If it was wrong, it will go to the second stack. The next card from the first stack will be presented.
+    </p>
+    <p>
+      Every once in a while, you'll get a card from the second (wrong) or third (correct) stack. If you had it wrong the first time, maybe you'll have it right this time. If you had it right the first time and now again, the card will go to the fourth deck.
+    </p>
+    <p>
+      The ultimate goal is to have all the cards in the done stack. To do so, all cards have to be answered right to consecutive times (so if the first time was right and the second time was wrong, then you need to answer it two times right again).
+    </p>
+    <p>
+      Ofcourse it is easy to cheat. Bit you'll be cheating on yourself won't you?
+    </p>
+    <p><strong>
+      Changing languages
+    </strong></p>
+    <p>
+      You can change the 'to' and 'from' languages in the settings by tapping on the language names.
+    </p>
+    <p><strong>
+      Adding / removing categories
+    </strong></p>
+    <p>
+      In the settings you can select the categories you want to play. Over time, new categories will be added.
+    </p>
+    <p><strong>
+      Playing offline
+    </strong></p>
+    <p>
+      Will folow...
+    </p>
+      
   </div>
 </template>
 
